52c2ef1951b29f01870da83ec7cae2881aa2806e
rokn <amindov@abv.bg>
2016-02-01 01:08:17 +0200

    Deleted some o files

--- Debug/OpenGL3Learning_HelperMethods.cpp.o.d
+++
@@ -1,8 +0,0 @@
-Debug/OpenGL3Learning_HelperMethods.cpp.o: \
- ../../OpenGL3Learning/OpenGL3Learning/HelperMethods.cpp \
- ../../OpenGL3Learning/OpenGL3Learning/HelperMethods.h \
- F:/Google\ Drive/Programming/OpenGL/glew-1.13.0/include/GL/glew.h
-
-../../OpenGL3Learning/OpenGL3Learning/HelperMethods.h:
-
-F:/Google\ Drive/Programming/OpenGL/glew-1.13.0/include/GL/glew.h:
